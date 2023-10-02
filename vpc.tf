dd024f1ca937a612cb0de2a32f0af0140d682f04
Victor <victorjgo86@gmail.com>
2023-10-02 18:08:42 +0100

    Modificacion VPC

--- vpc.tf
+++ vpc.tf
@@ -72,7 +72,6 @@ resource "aws_subnet" "private_subnet" {
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
     }
 
     tags = {
